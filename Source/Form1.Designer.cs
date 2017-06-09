dc06d15ad9dd35b00be51e862a9a78f6573878cc
Nikola Jovanovic <nik_jov_92@hotmail.com>
2017-06-09 12:46:58 +0200

    Added listViewControl

--- Source/Form1.Designer.cs
+++ Source/Form1.Designer.cs
@@ -29,6 +29,14 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.listProjekti = new System.Windows.Forms.ListView();
+            this.projekatID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.projekatNaziv = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.projekatGodina = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.projekatTip = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.projekatZavrsen = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.projekatGrupni = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.projekatPredmet = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // button1
@@ -37,18 +45,73 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Predmeti";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listProjekti
+            // 
+            this.listProjekti.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listProjekti.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.projekatID,
+            this.projekatNaziv,
+            this.projekatGodina,
+            this.projekatTip,
+            this.projekatZavrsen,
+            this.projekatGrupni,
+            this.projekatPredmet});
+            this.listProjekti.FullRowSelect = true;
+            this.listProjekti.GridLines = true;
+            this.listProjekti.Location = new System.Drawing.Point(93, 12);
+            this.listProjekti.Name = "listProjekti";
+            this.listProjekti.Size = new System.Drawing.Size(362, 237);
+            this.listProjekti.TabIndex = 1;
+            this.listProjekti.UseCompatibleStateImageBehavior = false;
+            this.listProjekti.View = System.Windows.Forms.View.Details;
+            // 
+            // projekatID
+            // 
+            this.projekatID.Text = "ID";
+            // 
+            // projekatNaziv
+            // 
+            this.projekatNaziv.Text = "Naziv";
+            this.projekatNaziv.Width = 117;
+            // 
+            // projekatGodina
+            // 
+            this.projekatGodina.Text = "Godina";
+            // 
+            // projekatTip
+            // 
+            this.projekatTip.DisplayIndex = 4;
+            this.projekatTip.Text = "Tip";
+            // 
+            // projekatZavrsen
+            // 
+            this.projekatZavrsen.DisplayIndex = 3;
+            this.projekatZavrsen.Text = "Zavrsen";
+            // 
+            // projekatGrupni
+            // 
+            this.projekatGrupni.Text = "Grupni";
+            // 
+            // projekatPredmet
+            // 
+            this.projekatPredmet.Text = "Predmet";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(467, 261);
+            this.Controls.Add(this.listProjekti);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -56,6 +119,14 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listProjekti;
+        private System.Windows.Forms.ColumnHeader projekatID;
+        private System.Windows.Forms.ColumnHeader projekatNaziv;
+        private System.Windows.Forms.ColumnHeader projekatGodina;
+        private System.Windows.Forms.ColumnHeader projekatTip;
+        private System.Windows.Forms.ColumnHeader projekatZavrsen;
+        private System.Windows.Forms.ColumnHeader projekatGrupni;
+        private System.Windows.Forms.ColumnHeader projekatPredmet;
     }
 }
 
--- Source/Form1.cs
+++ Source/Form1.cs
@@ -2,8 +2,7 @@
 using System.Windows.Forms;
 using NHibernate;
 using Source.Entiteti;
-
-
+using System.Collections.Generic;
 
 namespace Source
 {
@@ -16,16 +15,26 @@ namespace Source
 
         private void button1_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
             ISession s = DataLayer.GetSession();
-            Nastavnik n = s.Load<Nastavnik>(3);
-
-            MessageBox.Show(n.Ime);
-
-            foreach(Predmet p in n.Predmeti)
-            {
-                MessageBox.Show(p.Naziv);
-            }
+            IList<Projekat> projekti = s.QueryOver<Projekat>().List<Projekat>();
+            ListViewItem listViewItem;
             
+            foreach(Projekat projekat in projekti)
+            {
+                listViewItem = new ListViewItem(projekat.Id.ToString());
+                listViewItem.SubItems.Add(projekat.Naziv);
+                listViewItem.SubItems.Add(projekat.Godina.ToString());
+                listViewItem.SubItems.Add(projekat.Zavrsen);
+                listViewItem.SubItems.Add(projekat.Tip);
+                listViewItem.SubItems.Add(projekat.Grupni);
+                listViewItem.SubItems.Add(projekat.predmet.Naziv);
+                listProjekti.Items.Add(listViewItem);
+            }
 
             s.Close();
         }
